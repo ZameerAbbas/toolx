352101931a268814d5ca804e9840fbaea2e06405
ZameerAbbas <meloohunzai@gmail.com>
2024-02-03 15:12:36 +0500

    image compresser

--- src/pages/ImageCompresser/imageCompression.js
+++ src/pages/ImageCompresser/imageCompression.js
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { useState } from "react";
+import Compressor from "compressorjs";
 
 const ImageCompresser = () => {
+  const [compressedFile, setCompressedFile] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleCompressedUpload = (e) => {
+    setLoading(true);
+    const image = e.target.files[0];
+    new Compressor(image, {
+      quality: 0.2,
+      success: (compressedResult) => {
+        setCompressedFile(compressedResult);
+        setLoading(false);
+      },
+    });
+  };
+
+  const downloadCompressedImage = () => {
+    if (compressedFile) {
+      const downloadLink = document.createElement("a");
+      downloadLink.href = URL.createObjectURL(compressedFile);
+      downloadLink.download = "compressed_image.jpg";
+      downloadLink.click();
+    }
+  };
+
   return (
     <div>
-       ImageCompresser
+      <div>
+        <input
+          accept="image/*,capture=camera"
+          capture="camera"
+          type="file"
+          onChange={(event) => handleCompressedUpload(event)}
+        />
+      </div>
+      {loading ? (
+        <div>Loading...</div>
+      ) : compressedFile ? (
+        <div>
+          <button onClick={downloadCompressedImage}>Download Compressed Image</button>
+        </div>
+      ) : null}
     </div>
-  )
-}
+  );
+};
 
-export default ImageCompresser
+export default ImageCompresser;
